f4dde20e034679538f72c8cd0941db0fbec23c51
Crowdbotics <engineering@crowdbotics.com>
2022-09-01 08:06:45 +0000

    Update models

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import TdwedwdwdViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +10,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register("tdwedwdwd", TdwedwdwdViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Tdwedwdwd(models.Model):
+    "Generated Model"
+    dssc = models.BigIntegerField()
